9aa855c2989806833870254fbcb64c9512ebeb21
Cristian Granada <cristiansordo11@gmail.com>
2020-11-15 18:07:07 -0500

    Lab Geek 3

--- js/labGeek3.js
+++ js/labGeek3.js
@@ -1,44 +1,52 @@
 document.getElementById('ordenV').style.display="none"
 
-let tipoV;
-let propietario;
-let placa;
-let modelo;
-let cantidaddH;
-let descuento;
-let valor;
+let tipoV2='Carro';
+let propietario2;
+let placa2;
+let modelo2;
+let cantidaddH2;
+let descuento2;
+let valor2;
+let total2;
 
 function Calcular(){
     document.getElementById('paraqueadero').style.display="none"
     document.getElementById('ordenV').style.display="block"
 
-    cantidaddH = Number(document.getElementById('cantidadH').value);
-    tipoV = document.getElementById('TipoV');
-    propietario = document.getElementById('propietario');
-    placa = document.getElementById('placa');
-    modelo = document.getElementById('modelo');
+    
+    cantidaddH2 = Number(document.getElementById('cantidadH').value);
+    tipoV2 = document.getElementById('tipoV').value;
+    propietario2 = document.getElementById('propietario').value;
+    placa2 = document.getElementById('placa').value;
+    modelo2 = document.getElementById('modelo').value;
 
-    valor = valorVehiculo(cantidaddH);
-    descuento = Descuento(valor);
+    valor2 = valorVehiculo(cantidaddH2);
+    descuento2 = DescuentoT(valor2);
+
+    document.getElementById('descuento').value = descuento2;
+    document.getElementById('total').value = valor2 - descuento2;
 }
 
-var valorVehiculo = function(cantidaddH){
-    if (cantidaddH <= 2) {
-        valor = 5000*cantidaddH;
-    } else if (cantidaddH >2 && cantidaddH <=5){
-        valor = 5000*2 + 4000*(cantidaddH-2);
-    } else if (cantidaddH >5 && cantidaddH <=10){
-        valor = 5000*2 + 4000*3 + 3000*(cantidaddH-5);
+var valorVehiculo = function(cantidaddH2){
+    if (cantidaddH2 <= 2) {
+        valor2 = 5000*cantidaddH2;
+    } else if (cantidaddH2 >2 && cantidaddH2 <=5){
+        valor2 = 5000*2 + 4000*(cantidaddH2-2);
+    } else if (cantidaddH2 >5 && cantidaddH2 <=10){
+        valor2 = 5000*2 + 4000*3 + 3000*(cantidaddH2-5);
     } else {
-        valor = 5000*2 + 4000*3 + 3000*5 + 2000*(cantidaddH-10);
+        valor2 = 5000*2 + 4000*3 + 3000*5 + 2000*(cantidaddH2-10);
     }
-    return valor;
+    
+    return valor2;
 }
 
-function Descuento(valor){
-    if (tipoV == carro) {
-        descuento = valor*0.1;
+function DescuentoT(valor2) {
+    if (tipoV2 == 'Carro' || tipoV2 == 'carro') {
+        descuento2 = valor2*0.1;
     } else {
-        descuento = valor*0.15;
-    } return descuento;
-}
+        descuento2 = valor2*0.15;
+    } 
+    
+    return descuento2; 
+} 
